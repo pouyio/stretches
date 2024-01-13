fbb9c0455c36512cf0616bf088b2a69904dd7e6e
Vicente <pouyio@gmail.com>
2024-01-13 14:00:51 +0100

    remove logs

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,27 +1,13 @@
-import { Fragment, useState } from "react";
 import { useWakeLock } from "react-screen-wake-lock";
 
 export const Settings = () => {
-  const [logs, setLogs] = useState<string[]>([]);
-  const { request, release } = useWakeLock({
-    onError: (e) => setLogs((l) => [...l, `onError: ${JSON.stringify(e)}`]),
-    onRelease: (e) => setLogs((l) => [...l, `onRelease: ${JSON.stringify(e)}`]),
-    onRequest: () => setLogs((l) => [...l, `onRequested`]),
-  });
+  const { request, release } = useWakeLock();
 
   const onClick = () => {
-    request()
-      .then((e) => setLogs((l) => [...l, `requested: ${JSON.stringify(e)}`]))
-      .catch((e) =>
-        setLogs((l) => [...l, `error requested: ${JSON.stringify(e)}`])
-      );
+    request();
   };
   const onRelease = () => {
-    release()
-      .then((e) => setLogs((l) => [...l, `release: ${JSON.stringify(e)}`]))
-      .catch((e) => {
-        setLogs((l) => [...l, `error release: ${JSON.stringify(e)}`]);
-      });
+    release();
   };
 
   return (
@@ -30,14 +16,7 @@ export const Settings = () => {
       <button onClick={onClick}>Request</button>
       <button onClick={onRelease}>Release</button>
 
-      <code>
-        {logs.map((l, i) => (
-          <Fragment key={i}>
-            <br />
-            {l}
-          </Fragment>
-        ))}
-      </code>
+      <code></code>
     </>
   );
 };
