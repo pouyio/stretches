46b450086dcbb8d6a020f4e4709c04917a75d6de
Vicente <pouyio@gmail.com>
2024-04-09 17:40:12 +0200

    fixed image

--- src/assets/exercises.ts
+++ src/assets/exercises.ts
@@ -90,7 +90,7 @@ export const exercises: Record<string, Exercise> = {
       { name: "Caminata pato", repetitions: 10 },
     ],
   },
-  "Simplified": {
+  Simplified: {
     description: "3 series each",
     steps: [
       {
@@ -121,7 +121,7 @@ export const exercises: Record<string, Exercise> = {
       {
         name: "Shoulder isolation, lateral raise & rear delt fly",
         repetitions: 20,
-        img: "simplified/shoulder.PNG",
+        img: "simplified/lateral.PNG",
       },
       {
         name: "Quads, squat",
